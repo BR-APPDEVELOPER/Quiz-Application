946ce78ad11d738964bdd760d3bdb57f3f8af73a
Boopathy_raj <boopathyraj2204@gmail.com>
2025-02-08 18:57:02 +0530

    server file changes

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,9 @@ app.use(express.json());
 
 app.use('/api', userRoutes);
 
+app.get('/', (req, res)=>{
+    res.send("<h1>Hello world</h1>");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
